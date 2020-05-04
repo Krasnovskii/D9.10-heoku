f3210c2ca64ef81848f623d5fed1321bddbc965d
Sergei <krasnovskiisv@gmail.com>
2020-05-04 17:18:15 +0100

    first commit

---
+++ app/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from app.models import Post
+from django.contrib.auth.models import User
+
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name']
+
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    author = AuthorSerializer(required=False)
+
+    class Meta:
+        model = Post
+        fields = '__all__'
